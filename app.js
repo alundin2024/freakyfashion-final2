3526f275546d16c47552bd69606d3f596b54b229
Anton Lundin <lundin09@gmail.com>
2024-11-14 15:40:14 +0100

    Lagt in temporär js i app.js för test av admin-sida.

--- app.js
+++ app.js
@@ -22,6 +22,57 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/product-details", productDetailsRouter);
 
+app.get("/admin", function (req, res) {
+  // req = request-objektet innehåller information om anropet
+  // t.ex. att det är ett GET-arnop, och att URL är /register.
+
+  // res = response-objektet använder vi för att skicka tillbaka ett
+  // svar till klienten.
+  res.render("admin", {
+    title: "Administration",
+  });
+});
+
+// POST /register
+app.post("/admin", function (req, res) {
+  // Data som skickas från frontend (webbläsaren), som angivits
+  // i formuläret finns tillgänglig för oss att använda på backend
+  // (alltså här =) via egenskapen "body" på request-objektet (req).
+  const student = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    socialSecurityNumber: req.body.socialSecurityNumber,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    image: req.body.image,
+  };
+
+  // Vi använder "db"-objektet för att kommunicera med databasen.
+
+  // Vi skapar en statement - vi kör inte SQL-kommandot här, vi förbereder det enbart.
+  const insert = db.prepare(`
+    INSERT INTO students (
+      firstName, 
+      lastName,
+      socialSecurityNumber,
+      email,
+      phoneNumber,
+      image
+    ) VALUES (
+      @firstName, 
+      @lastName,
+      @socialSecurityNumber,
+      @email,
+      @phoneNumber,
+      @image
+     )
+  `);
+
+  // Nu kommer SQL-kommandot köras (INSERT INTO)
+  insert.run(student);
+
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));

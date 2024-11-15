8dc3b5e5ba9c8f9749d37afb761e940c80ed9557
Anton Lundin <lundin09@gmail.com>
2024-11-15 14:38:23 +0100

    Skapat db-objekt för product-list.db

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const Database = require("better-sqlite3");
+const db = new Database("./db/product-list.db", {
+  // Skriv ut SQL-kommandona som körs mot databasen
+  verbose: console.log,
+  // Skapa inte db-filen om den inte redan finns.
+  // fileMustExist: true,
+});
+
+
 var indexRouter = require("./routes/index");
 var productDetailsRouter = require("./routes/product-details");
 var adminRouter = require("./routes/admin");
@@ -42,38 +51,38 @@ app.post("/new", function (req, res) {
   // Data som skickas från frontend (webbläsaren), som angivits
   // i formuläret finns tillgänglig för oss att använda på backend
   // (alltså här =) via egenskapen "body" på request-objektet (req).
-  const student = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    socialSecurityNumber: req.body.socialSecurityNumber,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
+  const product = {
+    productName: req.body.productName,
+    description: req.body.description,
     image: req.body.image,
+    brand: req.body.brand,
+    sku: req.body.sku,
+    price: req.body.price,
   };
 
   // Vi använder "db"-objektet för att kommunicera med databasen.
 
   // Vi skapar en statement - vi kör inte SQL-kommandot här, vi förbereder det enbart.
   const insert = db.prepare(`
-    INSERT INTO students (
-      firstName, 
-      lastName,
-      socialSecurityNumber,
-      email,
-      phoneNumber,
-      image
+    INSERT INTO products (
+      productName, 
+      description,
+      image,
+      brand,
+      sku,
+      price
     ) VALUES (
-      @firstName, 
-      @lastName,
-      @socialSecurityNumber,
-      @email,
-      @phoneNumber,
-      @image
+      @productName, 
+      @description,
+      @image,
+      @brand,
+      @sku,
+      @price
      )
   `);
 
   // Nu kommer SQL-kommandot köras (INSERT INTO)
-  insert.run(student);
+  insert.run(product);
 
 });
 
